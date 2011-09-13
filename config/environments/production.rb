2bb35425b346807a6cda43b21acecc1830ad3e03
Nick Sieger <nick@nicksieger.com>
2011-09-13 14:39:53 -0500

    Prefix ::NewRelic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,4 +30,4 @@ config.action_mailer.raise_delivery_errors = false
 config.action_mailer.logger = nil
 
 # NewRelic isn't detecting Trinidad at the moment
-NewRelic::Agent.manual_start if defined?(JRUBY_VERSION)
+::NewRelic::Agent.manual_start if defined?(JRUBY_VERSION)
